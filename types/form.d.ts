043ab4fe3792dcd7f2028877d2a557fefa42f759
HuangMing <forzoom@163.com>
2020-09-04 13:26:15 +0800

    feat: 补充类型

--- types/form.d.ts
+++ types/form.d.ts
@@ -1,3 +1,13 @@
+export interface ValidateRule {
+    required?: boolean;
+    pattern?: RegExp;
+    trigger?: 'blur' | 'validate';
+    /** 最大长度 */
+    max?: number;
+    min?: number;
+    message?: string;
+}
+
 export interface FormBasicSectionMeta {
     type: string;
     key: string;
